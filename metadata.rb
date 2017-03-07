4e5848fc02d0b58b2a2c55420fbcdddc2db5ea77
Lonnie VanZandt <lonniev@gmail.com>
2017-03-06 19:59:15 -0700

    Indicate Ubuntu specificity

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'CC-BY'
 description      'Installs/Configures Upstart for Ubuntu Linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
+
+supports         'ubuntu'
